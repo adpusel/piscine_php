b72a5b48410ca06c38e3a8fe17319de7edbec94a
get <adrien.pusel@gmail.com>
2018-09-21 11:52:24 +0200

    tt est ok, il faut faire un dernier test et enlever les couleurs sur les liens

--- day_0/ex05/main.js
+++ day_0/ex05/main.js
@@ -1,6 +1,8 @@
 let cur_action = "rien"
 let cur_obj = "rien"
-let woman_diag = 'rien'
+let woman_diag = "rien"
+let player_diag = "rien"
+let dragon_diag = "rien"
 
 
 /*------------------------------------*\
@@ -28,6 +30,12 @@ function set_obj(name)
 function set_dialogue(value)
 {
   document.getElementById("dialogue").value = value
+}
+
+function set_description(name)
+{
+  cur_obj = name
+  document.getElementById("description").innerHTML = name
 }
 
 // set le click sur les obj et action
@@ -77,45 +85,85 @@ function set_all_click()
   /*------------------------------------*\
       page 42
   \*------------------------------------*/
-  document.getElementById("woman").onclick = function ()
+  if (document.getElementById("woman"))
   {
-	if (cur_obj === "towel" && cur_action === 'parler')
+	document.getElementById("woman").onclick = function ()
 	{
-	  set_dialogue("hey j'ai besion d'aide")
-	  woman_diag = "ok"
+	  if (cur_obj === "towel" && cur_action === "parler")
+	  {
+		set_dialogue("hey j'ai besion d'aide")
+		woman_diag = "ok"
+	  }
 	}
   }
 
-  document.getElementById("man").onclick = function ()
+  if (document.getElementById("man"))
   {
-	set_dialogue("fiche moi la paix")
+	document.getElementById("man").onclick = function ()
+	{
+	  set_dialogue("fiche moi la paix")
+	}
   }
 
+
+  /*------------------------------------*\
+      page player
+  \*------------------------------------*/
+  if (document.getElementById("joueur"))
+  {
+	document.getElementById("joueur").onclick = function ()
+	{
+	  if (cur_obj === "book" && cur_action === "regarder")
+	  {
+		set_dialogue("merci je ne l'avais pas celui la")
+		player_diag === "ok"
+	  }
+	}
+  }
+
+  /*------------------------------------*\
+      dragon
+  \*------------------------------------*/
+  if (document.getElementById("dragon"))
+  {
+	document.getElementById("dragon").onclick = function ()
+	{
+	  if (cur_obj === "brick" && cur_action === "utiliser")
+	  {
+		set_dialogue("dis 42")
+		dragon_diag = "ok"
+	  }
+	}
+  }
+
+
+  /*------------------------------------*\
+	  dialogue
+  \*------------------------------------*/
   document.getElementById("btn").onclick = function ()
   {
-	if (document.getElementById("dialogue").value === 'chicken')
-    set_dialogue("demande au bon joueur")
+	if (document.getElementById("dialogue").value === "towel" &&
+	  woman_diag === "ok")
+	{
+	  set_dialogue("demande au bon joueur")
+	}
+
+	if (document.getElementById("dialogue").value === "book" &&
+	  player_diag === "ok")
+	{
+	  set_dialogue("va voir le dragon_diag")
+	}
+
+	if ((document.getElementById("dialogue").value === "42" ||
+	  document.getElementById("dialogue").value === 42) &&
+	  dragon_diag === "ok")
+	{
+	  console.log("")
+	  set_dialogue("tu as gagner bravo")
+	}
   }
-}
 
-/*------------------------------------*\
-    page 42
-\*------------------------------------*/
-function dial()
-{
-  
 }
-
-/*------------------------------------*\
-    manage les descriptions
-\*------------------------------------*/
-function set_description(name)
-{
-  cur_obj = name
-  document.getElementById("description").innerHTML = name
-}
-
-// si j'ai la serviette et que je clique sur la dame elle me parle
 
 
 function display_action()
