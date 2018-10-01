aa4a43c3672f359bb3e0c192b772683cdf8bc440
Adrien PUSEL <adpusel@student.42.fr>
2018-10-01 10:56:33 +0200

    ok pour les date, rest a check que c'est le bon jour de la semaine

--- test.php
+++ test.php
@@ -8,11 +8,22 @@
 
 //$date = date_create_from_format('j-M-Y', '15-Feb-2009');
 
+
+//setlocale(LC_ALL, 'fr_FR');
+
+// essayer avec un jour qui n'existe pas
+
+// ne verifie pas que je jour est bien le bom jour,
+// je dois le faire a la main pour comparer les deux date
+
+//echo date_default_timezone_set("Europe/Paris");
+date_default_timezone_set("Europe/Paris");
+
+$format = 'D Y-m-d H:i:s';
+$date = DateTime::createFromFormat($format, 'Sun 2018-10-1 15:16:17');
+echo "Format: $format; " . $date->format('Y-m-d H:i:s') . "\n";
+
+
 //$date = date_create_from_format('j-M-Y', '12-07-1982 01:20:20');
-//$date = date_create_from_format('d-m-Y', '12-07-1982 01:20:20');
-//echo date_format($date, 'Y-m-d H:i:s');
-
-
-$format = 'Y-m-d H:i:s';
-//$date = DateTime::createFromFormat($format, '2009-02-15 15:16:17');
-//echo "Format: $format; " . $date->format('Y-m-d H:i:s') . "\n";
+$date = DateTime::createFromFormat('d-m-Y H:i:s', '12-07-1982 01:20:20');
+echo date_format($date, 'Y-m-d H:i:s');
