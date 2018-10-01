32f638cfad94ac39ff59bc0a9ad2d10818a97037
Adrien PUSEL <adpusel@student.42.fr>
2018-10-01 16:16:32 +0200

    save

---
+++ day_00/rendu/ex05/main.js
@@ -0,0 +1,211 @@
+let cur_action = "rien"
+let cur_obj = "rien"
+let woman_diag = "rien"
+let player_diag = "rien"
+let dragon_diag = "rien"
+
+
+/*------------------------------------*\
+    set les click sur les colomne
+\*------------------------------------*/
+function set_action(name)
+{
+  cur_action = name
+  document.getElementById("selected_action").innerHTML = name
+  console.log(name)
+  return (1)
+}
+
+function set_obj(name)
+{
+  if (cur_action === "prendre")
+  {
+	cur_obj = name
+	document.getElementById("selected_object").innerHTML = name
+	console.log(name)
+  }
+  return (1)
+}
+
+function set_dialogue(value)
+{
+  document.getElementById("dialogue").value = value
+}
+
+function set_description(name)
+{
+  cur_obj = name
+  document.getElementById("description").innerHTML = name
+}
+
+// set le click sur les obj et action
+function set_all_click()
+{
+  // je set tout les objets avec la bonne valeurs
+  document.getElementById("avancer").onclick = function ()
+  {
+	set_action("avancer")
+  }
+
+  document.getElementById("prendre").onclick = function ()
+  {
+	set_action("prendre")
+  }
+
+  document.getElementById("regarder").onclick = function ()
+  {
+	set_action("regarder")
+  }
+
+  document.getElementById("utiliser").onclick = function ()
+  {
+	set_action("utiliser")
+  }
+
+  document.getElementById("parler").onclick = function ()
+  {
+	set_action("parler")
+  }
+
+  document.getElementById("book").onclick = function ()
+  {
+	set_obj("book")
+  }
+
+  document.getElementById("towel").onclick = function ()
+  {
+	set_obj("towel")
+  }
+
+  document.getElementById("brick").onclick = function ()
+  {
+	set_obj("brick")
+  }
+
+  /*------------------------------------*\
+      page 42
+  \*------------------------------------*/
+  if (document.getElementById("woman"))
+  {
+	document.getElementById("woman").onclick = function ()
+	{
+	  if (cur_obj === "towel" && cur_action === "parler")
+	  {
+		set_dialogue("hey j'ai besion d'aide")
+		woman_diag = "ok"
+	  }
+
+	  if (cur_action === "regarder")
+	  {
+		set_description("c'est une femme qui est triste")
+	  }
+	}
+  }
+
+  if (document.getElementById("man"))
+  {
+	document.getElementById("man").onclick = function ()
+	{
+	  set_dialogue("fiche moi la paix")
+
+	  if (cur_action === "regarder")
+	  {
+		set_description("c'est un mec type pas gentil")
+	  }
+	}
+
+
+  }
+
+
+  /*------------------------------------*\
+      page player
+  \*------------------------------------*/
+  if (document.getElementById("joueur"))
+  {
+	document.getElementById("joueur").onclick = function ()
+	{
+	  if (cur_obj === "book" && cur_action === "regarder")
+	  {
+		set_dialogue("merci je ne l'avais pas celui la")
+		player_diag = "ok"
+	  }
+
+	  if (cur_action === "regarder")
+	  {
+		set_description("c'est un mec qui joue au carte")
+	  }
+	}
+  }
+
+  /*------------------------------------*\
+      dragon
+  \*------------------------------------*/
+  if (document.getElementById("dragon"))
+  {
+	document.getElementById("dragon").onclick = function ()
+	{
+	  if (cur_obj === "brick" && cur_action === "utiliser")
+	  {
+		set_dialogue("dis 42")
+		dragon_diag = "ok"
+	  }
+
+	  if (cur_action === "regarder")
+	  {
+		set_description("c'est un dragon")
+	  }
+	}
+
+
+  }
+
+
+  /*------------------------------------*\
+	  dialogue
+  \*------------------------------------*/
+  document.getElementById("btn").onclick = function ()
+  {
+	if (document.getElementById("dialogue").value === "towel" &&
+	  woman_diag === "ok")
+	{
+	  set_dialogue("demande au bon joueur")
+	}
+
+	if (document.getElementById("dialogue").value === "book" &&
+	  player_diag === "ok")
+	{
+	  set_dialogue("va voir le dragon ")
+	}
+
+	if ((document.getElementById("dialogue").value === "42" ||
+	  document.getElementById("dialogue").value === 42) &&
+	  dragon_diag === "ok")
+	{
+	  console.log("")
+	  set_dialogue("tu as gagner bravo")
+	}
+  }
+
+}
+
+
+function display_action()
+{
+  // // init les valeur
+  // var para = document.createElement("p")
+  // para.classList.add("selected_object")
+  //
+  // // creer et ajoutele texte
+  // var node = document.createTextNode("This is a new paraasdfasdfasfdasfasfdasfasdfasdfasdfasdfasdfgraph.")
+  // para.appendChild(node)
+  //
+  // // l'ajoute a la page
+  // var element = document.body.appendChild(para)
+}
+
+display_action()
+
+// load
+set_all_click()
+
