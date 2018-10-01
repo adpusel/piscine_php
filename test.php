0dae9e5177db9a5c49038c17d2489b3582e4c80f
Adrien PUSEL <adpusel@student.42.fr>
2018-10-01 09:28:56 +0200

    ajout fichier test pour les heures

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adpusel
+ * Date: 10/1/18
+ * Time: 8:55 AM
+ */
+
+//$date = date_create_from_format('j-M-Y', '15-Feb-2009');
+
+//$date = date_create_from_format('j-M-Y', '12-07-1982 01:20:20');
+//$date = date_create_from_format('d-m-Y', '12-07-1982 01:20:20');
+//echo date_format($date, 'Y-m-d H:i:s');
+
+
+$format = 'Y-m-d H:i:s';
+//$date = DateTime::createFromFormat($format, '2009-02-15 15:16:17');
+//echo "Format: $format; " . $date->format('Y-m-d H:i:s') . "\n";
