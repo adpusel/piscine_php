8ea74eb6c3bd8d10742cf925825467fe4c3d3792
Adrien PUSEL <adpusel@student.42.fr>
2018-10-04 16:29:15 +0200

    fix sur photo qui marche maintenant tout le temps

--- day_02/ex04/photos.php
+++ day_02/ex04/photos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 /**
  * Created by PhpStorm.
@@ -18,36 +19,32 @@
 \*------------------------------------*/
 $reg_http = "#http(s)?://#";
 $valide_link =
-  "/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/";
+    "/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/";
 
 /*------------------------------------*\
     input
 \*------------------------------------*/
-if ($argc !== 2)
-{
-  exit(" no arguments");
+if ($argc !== 2) {
+    exit(" no arguments");
 }
 
 // check l'address
 $address = $argv[1];
 echo $address;
-if (preg_match($valide_link, $address) === 0)
-{
-  exit(" wrong link");
+if (preg_match($valide_link, $address) === 0) {
+    exit(" wrong link");
 }
 
 // get le name du directory et le make
 $name_file = preg_replace($reg_http, "", $address);
-if (mkdir($name_file) === false)
-{
-  exit(" not able to create directory");
+if (mkdir($name_file) === false) {
+    exit(" not able to create directory");
 }
 
 // dl le file
 $html = file_get_contents($address);
-if ($file === false)
-{
-  exit("link not working");
+if ($file === false) {
+    exit("link not working");
 }
 
 /*------------------------------------*\
@@ -60,42 +57,36 @@ $tab_link = $tab_link[0];
 
 
 print_r($tab_link);
-foreach ($tab_link as $item)
-{
+foreach ($tab_link as $item) {
 
-  // clean le devant de l'url
-  while ($item[0] === '/')
-  {
-	$item = substr($item, 1);
-  }
+    // clean le devant de l'url
+    while ($item[0] === '/') {
+        $item = substr($item, 1);
+    }
 
-  // clean le https?
-  $item = preg_replace("#^(https?:\/\/)?#", "", $item);
+    if (preg_match("#^(https?:\/\/)#", $item) === 0)
+    {
+        // clean si address
+        $item = preg_replace("#www[^/]+.#", "", $item);
 
-  // clean si address
-  $item = preg_replace("#www[^/]+.#", "", $item);
-
-  $item = "$address/$item";
+        $item = "$address/$item";
+    }
 
 //  echo "$item\n";
 
-  // get le file
-  $file = file_get_contents($item);
-  if ($file !== false)
-  {
-	$nom_file = preg_replace($reg_http, "", $item);
-	$nom_file = explode('/', $nom_file);
+    // get le file
+    $file = file_get_contents($item);
+    if ($file !== false) {
+        $nom_file = preg_replace($reg_http, "", $item);
+        $nom_file = explode('/', $nom_file);
 
-	// set ptr to end of tab
-	$nom_file = end($nom_file);
-	if (file_put_contents("$name_file/$nom_file", $file) === false)
-	{
-	  echo "$file --> isn't downloaded\n";
-	}
-	echo "$name_file/$nom_file\n";
-  }
-  else
-  {
-	echo "$file --> isn't downloaded\n";
-  }
+        // set ptr to end of tab
+        $nom_file = end($nom_file);
+        if (file_put_contents("$name_file/$nom_file", $file) === false) {
+            echo "$file --> isn't downloaded\n";
+        }
+        echo "$name_file/$nom_file\n";
+    } else {
+        echo "$file --> isn't downloaded\n";
+    }
 }
