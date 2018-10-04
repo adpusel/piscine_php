faa5c37ebf73c344769dc1487c87d46a934ede92
Adrien PUSEL <adpusel@student.42.fr>
2018-10-04 16:36:10 +0200

    last save jour 2

--- day_02/ex05/deni.php
+++ day_02/ex05/deni.php
@@ -5,7 +5,7 @@
  * Date: 04/10/2018
  * Time: 11:44
  */
- 
+
 
 $tab_dico = false;
 $header = false;
@@ -15,48 +15,43 @@ if ($argc != 3 || file_exists($argv[1]) === false)
 
 if (($handle = fopen($argv[1], "r")) !== FALSE) {
 
-  while (($line = fgetcsv($handle, 1000, ";")) !== FALSE) {
+    while (($line = fgetcsv($handle, 1000, ";")) !== FALSE) {
 
-    // init les tab et check le header
-    if ($tab_dico === false)
-	{
+        // init les tab et check le header
+        if ($tab_dico === false) {
 
-	  $tab_dico = $line;
-	  foreach ($tab_dico as $index => $item)
-	  {
-		if ($item === $argv[2])
-		    $header = $index;
-	  }
-	  if ($header === false)
-	      exit();
+            $tab_dico = $line;
+            foreach ($tab_dico as $index => $item) {
+                if ($item === $argv[2])
+                    $header = $index;
+            }
+            if ($header === false)
+                exit();
 
-	  $size = count($tab_dico);
+            $size = count($tab_dico);
 
-	  continue;
-	}
+            continue;
+        }
 
-	// build les tab;
-	foreach ($line as $index => $item)
-	{
-	  ${$tab_dico[$index]}[$line[$header]] = $item;
-	}
+        // build les tab;
+        foreach ($line as $index => $item) {
+            ${$tab_dico[$index]}[$line[$header]] = $item;
+        }
 
-  }
-  fclose($handle);
-}
-else
-  exit();
+    }
+    fclose($handle);
+} else
+    exit();
 
 while (1) {
-  $line;
+    $line;
 
-  echo "Entrez votre commande: ";
+    echo "Entrez votre commande: ";
 
-  $line = fgets(STDIN);
-  if (feof(STDIN) == true)
-	break;
-
-  eval($line);
+    $line = fgets(STDIN);
+    if (feof(STDIN) == true)
+        break;
+    if ($line)
+        eval($line);
 }
 
-fclose($stdin);
