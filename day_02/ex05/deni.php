c84d64c1de2a5e062c51abd3c26037a5294ad19c
get <adrien.pusel@gmail.com>
2018-10-04 14:33:39 +0200

    le 05 est presque fini :)

---
+++ day_02/ex05/deni.php
@@ -0,0 +1,63 @@
+#!/usr/bin/php
+<?php
+/**
+ * User: adpusel
+ * Date: 04/10/2018
+ * Time: 11:44
+ */
+ 
+
+$tab_dico = false;
+$header = false;
+
+if ($argc != 3 || file_exists($argv[1]) === false)
+    exit();
+
+if (($handle = fopen($argv[1], "r")) !== FALSE) {
+
+  while (($line = fgetcsv($handle, 1000, ";")) !== FALSE) {
+
+    // init les tab et check le header
+    if ($tab_dico === false)
+	{
+
+	  $tab_dico = $line;
+	  foreach ($tab_dico as $index => $item)
+	  {
+		if ($item === $argv[2])
+		    $header = $index;
+	  }
+	  if ($header === false)
+	      exit();
+
+	  $size = count($tab_dico);
+
+	  continue;
+	}
+
+	// build les tab;
+	foreach ($line as $index => $item)
+	{
+	  ${$tab_dico[$index]}[$line[$header]] = $item;
+	}
+
+  }
+  fclose($handle);
+}
+else
+  exit();
+
+while (1) {
+  $line;
+  $last_char;
+
+  echo "Entrez votre commande: ";
+
+  $line = fgets(STDIN);
+  if (feof(STDIN) == true)
+	break;
+//  preg_replace("#'#", '\\\'', $line);
+  eval($line);
+}
+
+fclose($stdin);
