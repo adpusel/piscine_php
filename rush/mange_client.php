7613075f31bdbf4199cd42ef092cdb24e9b7978c
get <adrien.pusel@gmail.com>
2018-10-06 08:55:29 +0200

    save

---
+++ rush/mange_client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: adpusel
+ * Date: 06/10/2018
+ * Time: 08:14
+ */
+
+$ei;
+// les clients sont contituer de :
+	/**
+	client :
+	 * 	panier: [ les id des produit]
+	 *	mdp
+	 * 	droit
+	 *
+
+
+
+	 */
