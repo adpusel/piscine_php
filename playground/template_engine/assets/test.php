03ef9d30573f9374eac40ef40a0afa96647c735d
get <adrien.pusel@gmail.com>
2018-10-09 03:08:43 +0200

    les template simple sont ok

--- playground/template_engine/assets/test.php
+++ playground/template_engine/assets/test.php
@@ -16,117 +16,98 @@ $file = file_get_contents("test.twig");
 // si c'est un tab ca fait ca :);
 
 
-
-
 // charger les boucle php
 // si je genere un new file php c'est pas bon ca
 // je ne sais pas comment faire les loop
 
 
-//$file = preg_replace_callback("#\{\{(.*)\}\}#", function ($ar) {
+//function ($ar)
+//{
+//  $tab = array("super" => "mager", "name" => "toto", "tybus" => [
+//	["link" => "super_link",
+//	  "name" => "ce truc est cool"
+//	], ["link" => "super_link",
+//	  "name" => "ce truc est cool"]]
+//  );
 //
-//  $tab = array("super" => "mager", "name" => "toto");
+//  $tab_out = array();
+//  $name = "tybus";
 //
-//	foreach ($tab as $key => $item)
+//  foreach ($name as $key => $item)
+//  {
+//
+//	if ($key === trim($ar[1]))
 //	{
-//
-//	  	if ($key === trim($ar[1]))
-//		{
-//		  echo "$item\n";
-//		  return ($item);
-//		}
+//	  echo "$item\n";
+//	  return ($item);
 //	}
+//  }
+//}
 //
-//  //trim
-////  var_dump($ar);
-//}, $file);
+//;
 
 
-/*------------------------------------*\
+$tab = array("super" => "mager", "name" => "toto");
 
-\*------------------------------------*/
+function utils_simple_template($match, $tab)
+{
+  foreach ($tab as $key => $item)
+  {
 
-
-$file = preg_replace_callback("#\{\{(.*)\}\}#", function ($ar) {
-
-  $tab = array("super" => "mager", "name" => "toto");
-
-	foreach ($tab as $key => $item)
+	if ($key === trim($match[1]))
 	{
+	  echo "$item\n";
+	  return ($item);
+	}
+  }
+}
 
-	  	if ($key === trim($ar[1]))
+function simple_template($data, $tab)
+{
+  $data = preg_replace_callback("#\{\{(.*)\}\}#",
+
+	function ($match) use ($tab) {
+
+	  foreach ($tab as $key => $item)
+	  {
+
+		if ($key === trim($match[1]))
 		{
 		  echo "$item\n";
 		  return ($item);
 		}
-	}
+	  }
+	}, $data);
 
-  //trim
-//  var_dump($ar);
-}, $file);
-
-
+  return $data;
+}
 
 
-//$file = preg_replace_callback("#\{\{(.*)\}\}#", function ($ar) {
+echo simple_template($file, $tab);
+
+
+
+//function template_simple($file, $data)
+//{
+//  $file = preg_replace_callback("#\{%(.*)%\}{2}#", function ($ar) {
 //
-//  $tab = array("super" => "mager", "name" => "toto");
+//
+//	$tab = array("super" => "mager", "name" => "toto", "tybus" => [
+//	  "link" => "super_link",
+//	  "name" => "ce truc est cool"
+//	]);
 //
 //	foreach ($tab as $key => $item)
 //	{
 //
-//	  	if ($key === trim($ar[1]))
-//		{
-//		  echo "$item\n";
-//		  return ($item);
-//		}
+//	  echo "$item\n";
 //	}
 //
-//  //trim
-////  var_dump($ar);
-//}, $file);
+//	trim
+//	var_dump($ar[1]);
+//  }, $file);
+//  echo $file;
+//}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// si j'ai un tab de tab, comment je l'echap ?
-// je check correctement mais avec quoi
-// faire les template a partir du code sql s
-
-// si j'ai un tab d'elements, je dois le faire
-
-// get file ok
-// juste faire un eco, mais avec ce truc si j'ai des injections de code comment
-// je me protege ? haha !! il me faut faire plus attention
-echo $file;
-
-// je peux genere la boocle en catchant ce que j'ai dedans et en
-// le faisant ensuite passer dans la seconde boucle
-// if wooblr -->
-// if if --> je fais le code avec le tab, tout let temps
-// // pas de if pour le moment :)
