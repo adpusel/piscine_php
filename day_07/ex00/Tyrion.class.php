33ba3bab880b7fb167a412b055cc3e5813f7ab86
Adrien PUSEL <adpusel@student.42.fr>
2018-10-11 11:45:20 +0200

    ex 01 et 02 ok

---
+++ day_07/ex00/Tyrion.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adpusel
+ * Date: 10/11/18
+ * Time: 11:36 AM
+ */
+
+class Tyrion extends Lannister
+{
+  public function __construct()
+  {
+	parent::__construct();
+	print "My name is Tyrion\n";
+  }
+
+  public function getSize()
+  {
+	return "Short";
+  }
+
+  public function houseMotto()
+  {
+	return "Hear me roar!";
+  }
+}
---
+++ day_07/ex01/Greyjoy.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adpusel
+ * Date: 10/11/18
+ * Time: 11:41 AM
+ */
+
+class Greyjoy
+{
+  protected $familyMotto = "We do not sow";
+}
